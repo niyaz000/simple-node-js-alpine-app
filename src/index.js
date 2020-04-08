4d819a181c6a577fc9e97dcf556e0206a881d96a
Niyaz K <niyazk2991@gmail.com>
2020-04-08 22:12:29 +0530

    remove white space

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(PORT, () => {
   console.log(`app started on port ${PORT}`);
 })
